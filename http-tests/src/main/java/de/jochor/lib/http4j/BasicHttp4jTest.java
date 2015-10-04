86e88c21d8ef345f89d5b79b74b77e02e2f812b8
Jochen Hormes <jochen@jochor.de>
2015-10-04 19:47:42 +0200

    Added mockserver as HTTP server for jUnit tests. Maybe WireMock would be a good alternative. #4

--- http-tests/src/main/java/de/jochor/lib/http4j/BasicHttp4jTest.java
+++ http-tests/src/main/java/de/jochor/lib/http4j/BasicHttp4jTest.java
@@ -1,12 +1,22 @@
 package de.jochor.lib.http4j;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URI;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+
+import de.jochor.lib.http4j.model.GetRequest;
 
 /**
- * Test for JSON Binding Service adapter implementations.
+ * Test for HTTP client adapter implementations.
  *
  * <p>
  * <b>Started:</b> 2015-10-03
@@ -34,9 +44,27 @@ public class BasicHttp4jTest {
 		Assert.assertNotNull(httpClient);
 	}
 
+	@Ignore
 	@Test
-	public void test() {
-		// TODO
+	public void testSimpleGet() {
+		String testContent = "test content";
+		// MockServer mockServer = new MockServer(8080);
+		MockServerClient mockServerClient = new MockServerClient("localhost", 8080);
+		mockServerClient.when(new HttpRequest().withMethod("GET").withPath("/")).respond(new HttpResponse().withBody(testContent));
+
+		GetRequest request = new GetRequest(URI.create("TODO"));
+
+		String content = httpClient.get(request);
+
+		Assert.assertEquals(testContent, content);
+	}
+
+	private static int pickOpenPort() {
+		try (final ServerSocket socket = new ServerSocket(0)) {
+			return socket.getLocalPort();
+		} catch (IOException ioe) {
+			throw new RuntimeException(ioe);
+		}
 	}
 
 }
---
+++ http4j-apache/src/test/java/de/jochor/lib/http4j/apache/BasicHttp4jTest.java
@@ -0,0 +1,14 @@
+package de.jochor.lib.http4j.apache;
+
+/**
+ * Test for HTTP client adapter implementations.
+ *
+ * <p>
+ * <b>Started:</b> 2015-10-03
+ * </p>
+ *
+ * @author Jochen Hormes
+ *
+ */
+public class BasicHttp4jTest extends de.jochor.lib.http4j.BasicHttp4jTest {
+}
